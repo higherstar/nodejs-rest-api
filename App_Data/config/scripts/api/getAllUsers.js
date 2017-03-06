2e52fa1dd7d2e27422eb9b773b9091eb35bcfc43
Higher Star <higherstar@outlook.com>
2017-03-06 14:00:41 +0000

    initialize

---
+++ App_Data/config/scripts/api/getAllUsers.js
@@ -0,0 +1,35 @@
+
+
+exports.get = function(request, response) {
+        console.log("getAllUsers(v8)===============");
+    
+    if (request.user.level == 'anonymous') {
+        response.send(statusCodes.UNAUTHORIZED, null);
+        return;
+    }
+    var errorHandler = function (err) {
+        console.error(err);
+        response.send(statusCodes.INTERNAL_SERVER_ERROR, err);
+    };
+            
+            
+            var bbGapi = require('../shared/bbGraphAPI.js');
+
+            bbGapi.getAADToken(function (err, access_token) {
+                if (err) errorHandler(err);
+                else{
+                    console.log("getAllUsers: access_token=" + access_token);
+                    
+                    bbGapi.getAllUsers(access_token, function (err, user_info) {
+                        if (err) errorHandler(err);
+                        else{
+                            console.log("getAllUsers: user_info=" + user_info);
+                            response.send(statusCodes.OK, user_info);
+                        } 
+                    });  
+                } 
+                    
+            });
+ 
+    
+};
